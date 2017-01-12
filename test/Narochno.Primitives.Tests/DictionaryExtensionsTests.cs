12d672fe7bc2b5bccb13c038d8e636cce940b0a6
Alan Edwardes <alan@alanedwardes.com>
2017-01-12 00:07:27 +0000

    Adds some dictionary helpers.

---
+++ test/Narochno.Primitives.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Narochno.Primitives.Tests
+{
+    public class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void TestGetOptionalExists()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "test", "testing" }
+            };
+
+            Assert.Equal("testing", dict.GetOptional("test").Value);
+        }
+
+        [Fact]
+        public void TestGetOptionalNotExists()
+        {
+            var dict = new Dictionary<string, string>();
+
+            Assert.False(dict.GetOptional("test").HasValue);
+        }
+
+        [Fact]
+        public void TestGetNullableExists()
+        {
+            var dict = new Dictionary<string, int>
+            {
+                { "test", 1 }
+            };
+
+            Assert.Equal(1, dict.GetNullable("test").Value);
+        }
+
+        [Fact]
+        public void TestGetNullableNotExists()
+        {
+            var dict = new Dictionary<string, int>();
+
+            Assert.False(dict.GetNullable("test").HasValue);
+        }
+    }
+}
