d4622720df6417989a71e177bed8f77b4b9666d4
Alan Edwardes <alan@alanedwardes.com>
2017-02-05 14:48:56 +0000

    Adds JSON EnumStringConverter

---
+++ src/Narochno.Primitives.Json/EnumStringConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Reflection;
+using Narochno.Primitives.Parsing;
+
+namespace Narochno.Primitives.Json
+{
+    public class EnumStringConverter : JsonConverter
+    {
+        private IParserLibrary parserLibrary = DefaultParserLibrary.Instance;
+
+        public EnumStringConverter()
+        {
+        }
+
+        public EnumStringConverter(IParserLibrary parserLibrary)
+        {
+            this.parserLibrary = parserLibrary;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.GetNullableUnderlyingType().GetTypeInfo().IsEnum;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType.IsNullable() && reader.Value == null)
+            {
+                return existingValue;
+            }
+
+            return parserLibrary.GetParser(objectType.GetNullableUnderlyingType()).Parse(reader.Value?.ToString());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Narochno.Primitives/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace Narochno.Primitives
+{
+    public static class TypeExtensions
+    {
+        public static bool IsNullable(this Type type)
+        {
+            return type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        public static Type GetNullableUnderlyingType(this Type type)
+        {
+            if (type.IsNullable())
+            {
+                return Nullable.GetUnderlyingType(type);
+            }
+
+            return type;
+        }
+    }
+}
