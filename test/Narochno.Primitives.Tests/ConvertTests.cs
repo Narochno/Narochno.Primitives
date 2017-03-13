3d47a0922a715e01a1e3e68509912992232d563a
Alan Edwardes <alan@alanedwardes.com>
2017-03-13 21:07:08 +0000

    Breaks test

--- test/Narochno.Primitives.Tests/ConvertTests.cs
+++ test/Narochno.Primitives.Tests/ConvertTests.cs
@@ -7,7 +7,7 @@ namespace Narochno.Primitives.Parsing.Tests
         [Fact]
         public void Parse()
         {
-            Assert.Equal(ushort.MaxValue, Convert<ushort>.Parse("65535"));
+            Assert.Equal(ushort.MaxValue, Convert<ushort>.Parse("hello"));
             Assert.Equal(float.MinValue, Convert<float>.Parse("-3.40282347E+38"));
             Assert.Equal(long.MaxValue, Convert<long>.Parse("9223372036854775807"));
         }
